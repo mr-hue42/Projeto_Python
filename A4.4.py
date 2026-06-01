1f34bcbcfec92144ac045cff16b274cc782c9231
mr-hue42 <filipegomes.mr@gmail.com>
2026-06-01 07:49:59 +0100

    Adicionar comentários

--- A4.4.py
+++ A4.4.py
@@ -1,7 +1,10 @@
+#pedir ao utilizador para indicar dois números
 n1= int(input("Indique um número: "))
 n2= int(input("Indique um de novo um número: "))
-
+#avaliar se os números indicados são iguais
 if n1==n2:
+#caso a condição se confirme, enviar a mensagem abaixo
     print("Os números são iguais")
 else:
+#caso a condição não se confirme, enviar a mensagem abaixo
     print("Os números são diferentes")
